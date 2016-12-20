ba8034d9aa22846266537493f8aaf014c0f59e11
d.ohne <d.ohne@DESKTOP-H4I0QPT>
2016-12-20 15:13:29 +0100

    Adds Controller for Products, Waiters and Categories

---
+++ Theke/Theke/Controllers/WaitersController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Theke.Models;
+
+namespace Theke.Controllers
+{
+    public class WaitersController : Controller
+    {
+        private thekeEntities db = new thekeEntities();
+
+        // GET: Waiters
+        public ActionResult Index()
+        {
+            var waiter = db.Waiter.Include(w => w.AspNetUsers);
+            return View(waiter.ToList());
+        }
+
+        // GET: Waiters/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Waiter waiter = db.Waiter.Find(id);
+            if (waiter == null)
+            {
+                return HttpNotFound();
+            }
+            return View(waiter);
+        }
+
+        // GET: Waiters/Create
+        public ActionResult Create()
+        {
+            ViewBag.UserID = new SelectList(db.AspNetUsers, "Id", "Email");
+            return View();
+        }
+
+        // POST: Waiters/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "WaiterID,VName,LName,Nicname,UserID")] Waiter waiter)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Waiter.Add(waiter);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.UserID = new SelectList(db.AspNetUsers, "Id", "Email", waiter.UserID);
+            return View(waiter);
+        }
+
+        // GET: Waiters/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Waiter waiter = db.Waiter.Find(id);
+            if (waiter == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.UserID = new SelectList(db.AspNetUsers, "Id", "Email", waiter.UserID);
+            return View(waiter);
+        }
+
+        // POST: Waiters/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "WaiterID,VName,LName,Nicname,UserID")] Waiter waiter)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(waiter).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.UserID = new SelectList(db.AspNetUsers, "Id", "Email", waiter.UserID);
+            return View(waiter);
+        }
+
+        // GET: Waiters/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Waiter waiter = db.Waiter.Find(id);
+            if (waiter == null)
+            {
+                return HttpNotFound();
+            }
+            return View(waiter);
+        }
+
+        // POST: Waiters/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Waiter waiter = db.Waiter.Find(id);
+            db.Waiter.Remove(waiter);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
