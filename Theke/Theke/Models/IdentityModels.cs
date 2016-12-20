40502eb7a00a18a275afc2cf2cd76245240a19c1
d.ohne <d.ohne@DESKTOP-H4I0QPT>
2016-12-20 14:34:31 +0100

    changes egal

--- Theke/Theke/Models/IdentityModels.cs
+++ Theke/Theke/Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace Theke.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base( "IdentityConnection", throwIfV1Schema: false)
+            : base("DefaultConnection", throwIfV1Schema: false)
         {
         }
 
