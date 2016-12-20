fcec4d74e51ab8ba16c6a2cc0064157f12f6c657
postcatz <marco@postcatz.com>
2016-12-20 13:59:25 +0100

    Metadata für Product.

---
+++ Theke/Theke/Controllers/ProductsController.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Theke.Models;
+
+namespace Theke.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private thekeEntities db = new thekeEntities();
+
+        // GET: Products
+        public ActionResult Index()
+        {
+            var product = db.Product.Include(p => p.AspNetUsers).Include(p => p.AspNetUsers1).Include(p => p.AgeRating).Include(p => p.Category).Include(p => p.NutriantContent).Include(p => p.Producer).Include(p => p.ProductUnit);
+            return View(product.ToList());
+        }
+
+        // GET: Products/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = db.Product.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        // GET: Products/Create
+        public ActionResult Create()
+        {
+            ViewBag.CreatedByID = new SelectList(db.AspNetUsers, "Id", "Email");
+            ViewBag.ModifiedByID = new SelectList(db.AspNetUsers, "Id", "Email");
+            ViewBag.AgeRatingID = new SelectList(db.AgeRating, "AgeRatingID", "AgeRatingID");
+            ViewBag.CategorieID = new SelectList(db.Category, "CategoryID", "Name");
+            ViewBag.NutriantContentID = new SelectList(db.NutriantContent, "NutrinantContentID", "NutrinantContentID");
+            ViewBag.ProducerID = new SelectList(db.Producer, "ProducerID", "Name");
+            ViewBag.ProductUnitID = new SelectList(db.ProductUnit, "ProductUnitID", "Name");
+            return View();
+        }
+
+        // POST: Products/Create
+        // Aktivieren Sie zum Schutz vor übermäßigem Senden von Angriffen die spezifischen Eigenschaften, mit denen eine Bindung erfolgen soll. Weitere Informationen 
+        // finden Sie unter http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ProductID,IsActive,Name,Note,AgeRatingID,ProductUnitID,ProducerID,AlcoholAmount,NutriantContentID,EanNumber,Deposit,ItemAmount,CategorieID,MinPrice,MaxPricce,AveragePurchasePrice,CreatedByID,ModifiedByID,CreatedDate,ModfiedDate")] Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Product.Add(product);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.CreatedByID = new SelectList(db.AspNetUsers, "Id", "Email", product.CreatedByID);
+            ViewBag.ModifiedByID = new SelectList(db.AspNetUsers, "Id", "Email", product.ModifiedByID);
+            ViewBag.AgeRatingID = new SelectList(db.AgeRating, "AgeRatingID", "AgeRatingID", product.AgeRatingID);
+            ViewBag.CategorieID = new SelectList(db.Category, "CategoryID", "Name", product.CategorieID);
+            ViewBag.NutriantContentID = new SelectList(db.NutriantContent, "NutrinantContentID", "NutrinantContentID", product.NutriantContentID);
+            ViewBag.ProducerID = new SelectList(db.Producer, "ProducerID", "Name", product.ProducerID);
+            ViewBag.ProductUnitID = new SelectList(db.ProductUnit, "ProductUnitID", "Name", product.ProductUnitID);
+            return View(product);
+        }
+
+        // GET: Products/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = db.Product.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CreatedByID = new SelectList(db.AspNetUsers, "Id", "Email", product.CreatedByID);
+            ViewBag.ModifiedByID = new SelectList(db.AspNetUsers, "Id", "Email", product.ModifiedByID);
+            ViewBag.AgeRatingID = new SelectList(db.AgeRating, "AgeRatingID", "AgeRatingID", product.AgeRatingID);
+            ViewBag.CategorieID = new SelectList(db.Category, "CategoryID", "Name", product.CategorieID);
+            ViewBag.NutriantContentID = new SelectList(db.NutriantContent, "NutrinantContentID", "NutrinantContentID", product.NutriantContentID);
+            ViewBag.ProducerID = new SelectList(db.Producer, "ProducerID", "Name", product.ProducerID);
+            ViewBag.ProductUnitID = new SelectList(db.ProductUnit, "ProductUnitID", "Name", product.ProductUnitID);
+            return View(product);
+        }
+
+        // POST: Products/Edit/5
+        // Aktivieren Sie zum Schutz vor übermäßigem Senden von Angriffen die spezifischen Eigenschaften, mit denen eine Bindung erfolgen soll. Weitere Informationen 
+        // finden Sie unter http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ProductID,IsActive,Name,Note,AgeRatingID,ProductUnitID,ProducerID,AlcoholAmount,NutriantContentID,EanNumber,Deposit,ItemAmount,CategorieID,MinPrice,MaxPricce,AveragePurchasePrice,CreatedByID,ModifiedByID,CreatedDate,ModfiedDate")] Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(product).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.CreatedByID = new SelectList(db.AspNetUsers, "Id", "Email", product.CreatedByID);
+            ViewBag.ModifiedByID = new SelectList(db.AspNetUsers, "Id", "Email", product.ModifiedByID);
+            ViewBag.AgeRatingID = new SelectList(db.AgeRating, "AgeRatingID", "AgeRatingID", product.AgeRatingID);
+            ViewBag.CategorieID = new SelectList(db.Category, "CategoryID", "Name", product.CategorieID);
+            ViewBag.NutriantContentID = new SelectList(db.NutriantContent, "NutrinantContentID", "NutrinantContentID", product.NutriantContentID);
+            ViewBag.ProducerID = new SelectList(db.Producer, "ProducerID", "Name", product.ProducerID);
+            ViewBag.ProductUnitID = new SelectList(db.ProductUnit, "ProductUnitID", "Name", product.ProductUnitID);
+            return View(product);
+        }
+
+        // GET: Products/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = db.Product.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        // POST: Products/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Product product = db.Product.Find(id);
+            db.Product.Remove(product);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Theke/Theke/Models/MetaData/Product.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Theke.Models.Product
+{
+    [MetadataType(typeof(ProductMetaData))]
+    public partial class Product
+    {
+
+    }
+
+    public class ProductMetaData
+    {
+
+        [Display(Name ="ProduktID")]
+        public int ProductID { get; set; }
+
+        [Display(Name = "Aktiv")]
+        public Nullable<byte> IsActive { get; set; }
+
+        [Required]
+        [Display(Name = "Produktbezeichnung")]
+        [StringLength(250, MinimumLength = 1, ErrorMessage = "Das Feld mus mindesten 1 Zeichen enthalten")]
+        public string Name { get; set; }
+
+        [Display(Name = "Beschreibung")]
+        [StringLength(250, MinimumLength = 1, ErrorMessage = "Das Feld mus mindesten 1 Zeichen enthalten")]
+        public string Note { get; set; }
+
+
+        [Display(Name ="AltersfreigabeID")]
+        public int AgeRatingID { get; set; }
+
+        [Display(Name = "ProduktEinheitID")]
+        public int ProductUnitID { get; set; }
+
+        [Display(Name ="HerstellerID")]
+        public int ProducerID { get; set; }
+
+
+        [Display(Name = "Alkoholgehalt")]
+        public int AlcoholAmount { get; set; }
+
+
+        [Display(Name ="NährstoffGehaltID")]
+        public int NutriantContentID { get; set; }
+
+        [Display(Name ="Barcode")]
+        public Nullable<int> EanNumber { get; set; }
+
+
+        [Display(Name = "Pfand")]
+        public int Deposit { get; set; }
+
+        [Required]
+        [Display(Name ="Füllmenge")]
+        public int ItemAmount { get; set; }
+
+        [Required]
+        [Display(Name ="KategorieID")]
+        public int CategorieID { get; set; }
+
+        [Required]
+        [Display(Name ="Min Preis")]
+        public double MinPrice { get; set; }
+
+        [Required]
+        [Display(Name = "Max Preis")]
+        public double MaxPricce { get; set; }
+
+
+        [Required]
+        [Display(Name = "Durchschnittlicher Einkaufspreis")]
+        public Nullable<int> AveragePurchasePrice { get; set; }
+
+        [Display(Name ="Erstellt von")]
+        public string CreatedByID { get; set; }
+
+        [Display(Name = "Geändert von")]
+        public string ModifiedByID { get; set; }
+
+        [Display(Name = "Erstellt am")]
+        public System.DateTime CreatedDate { get; set; }
+
+        [Display(Name = "Geändert am")]
+        public System.DateTime ModfiedDate { get; set; }
+    }
+}
