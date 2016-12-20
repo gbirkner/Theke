5069c96fd3cbc28eb3b94540705533721d284973
mobermair <mike.obermair@gmail.com>
2016-12-20 15:52:47 +0100

    Erstellung TableView mit ersten button und Erstellung BarOrderView für einzelne Tische

---
+++ Theke/Theke/Controllers/OrderView/BarTablesController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+using Theke.Models;
+
+namespace Theke.Controllers.OrderView
+{
+    public class BarTablesController : Controller
+    {
+        // GET: BarTables
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+
+    
+}
