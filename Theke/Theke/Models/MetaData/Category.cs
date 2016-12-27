84ef7fe9d3839730bc272512bfe22798fb99ee57
d.ohne <d.ohne@DESKTOP-H4I0QPT>
2016-12-27 02:35:00 +0100

    category.cs under Metadata

--- Theke/Theke/Models/MetaData/Category.cs
+++ Theke/Theke/Models/MetaData/Category.cs
@@ -4,7 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
-namespace Theke.Models.MetaData {
+namespace Theke.Models {
 
     [MetadataType( typeof( CategoryMetaData ) )]
     public partial class Category {
